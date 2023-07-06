e4267ec850c264fce4662d6d8698ca9c1370ad51
MinkieYume <minkiedash@163.com>
2023-07-06 18:26:56 +0800

    owo

--- difficult_panel.gd
+++ difficult_panel.gd
@@ -13,6 +13,16 @@ func _on_focus_entered():
 	material.set_shader_parameter("alpha",1.0)
 	game_window.level_name = level_name
 
+func _unhandled_input(event):
+	if has_focus():
+		if event.is_action_pressed("ui_accept"):
+			pass
+		if event.is_action_pressed("ui_cancel"):
+			pass
+
+func switch_to_flayer_chose():
+	pass
+
 func _on_focus_exited():
 	material.set_shader_parameter("alpha",0.5)
 	toggle_audio.play()
